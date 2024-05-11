1a3d01b3dd6c6479dee4413a42e86934b88632f2
lucasherlon <lucasherlondsmc@gmail.com>
2024-05-11 20:09:20 -0300

    trabalhando com cifragem de arquivos

--- cript_and_descript_text.py
+++ cript_and_descript_text.py
@@ -3,8 +3,7 @@ from import_export_keys import import_public_key
 from Crypto.Cipher import PKCS1_OAEP
 import json
 import os
-import random
-import string
+
 
 usuarios = dict()
 mensagens = dict()
@@ -16,7 +15,10 @@ def cifragem(nome):
         return
     pub_key = usuarios[nome]['public_key']
     #carrega o conteudo do arq com o texto claro
-    mensagem = str(input('Digite a mensagem a ser cifrada: ')).encode('utf-8')
+    arquivo = str(input('Digite o nome do arquivo (sem extensao) com a mensagem a ser cifrada: '))
+    with open(f'{arquivo}.txt', 'rb') as file:
+        mensagem = file.read()
+    mensagem = mensagem.decode('utf-8').encode('utf-8')
 
     #importa a chave publica do destinatario
     chave_importada = RSA.import_key(open(pub_key).read())
@@ -24,22 +26,18 @@ def cifragem(nome):
     #criptografa a mensagem com a chave public do destinatario
     cipher = PKCS1_OAEP.new(chave_importada)
     mensagem_cifrada = cipher.encrypt(mensagem)
-    # criando um nome aleatorio para o arquivo da mensagem
-    string_length = 7
-    chars = string.ascii_letters + string.digits
-    random_string = ''.join(random.sample(chars, string_length))
     #jogando a mensagem cifrada em um novo arq
     if not os.path.exists('mensagens_cifradas'): #cria a pasta keys se ela nao existir
         os.makedirs('mensagens_cifradas')
-    with open(f'mensagens_cifradas/msg_cifrada_{random_string}.txt', 'wb') as file:
+    with open(f'mensagens_cifradas/{arquivo}_cifrada.txt', 'wb') as file:
         file.write(mensagem_cifrada)
-    if not os.path.exists('usuarios.json'): #cria o arquivo json se ele não existir
+    if not os.path.exists('mensagens.json'): #cria o arquivo json se ele não existir
             with open('mensagens.json', 'w') as jsonfile:
                 jsonfile.write('{}')
     with open('mensagens.json', 'r') as jsonfile:
         mensagens = json.load(jsonfile)
-        if random_string not in usuarios:
-            mensagens[random_string] = {'destinatario': f'{nome}'}
+        if arquivo not in mensagens:
+            mensagens[arquivo] = {'destinatario': f'{nome}', 'identificador': f'{arquivo}_cifrada.txt'}
     with open('mensagens.json', 'w') as jsonfile:
         json.dump(mensagens, jsonfile)
 
@@ -64,21 +62,13 @@ def decifragem(nome):
     #importa a chave privada do destinatario
     chave_importada = RSA.import_key(open(f'{priv_key}').read())
     #carrega as mensagens decifrando-as e mostrando na tela
-    for msg in minhas_mensagens:
-        with open(f'mensagens_cifradas/msg_cifrada_{msg}.txt', 'rb') as file:
-            mensagem_cifrada = file.read()
-            cipher = PKCS1_OAEP.new(chave_importada)
-            mensagem = cipher.decrypt(mensagem_cifrada)
-            print(mensagem.decode('utf-8'))
+    arquivo = str(input('Digite o nome do arquivo cifrado (sem extensão) com a mensagem a ser decifrada: '))
 
-
-
-
-'''
-    #jogando a mensagem decifrada em um novo arq
-    with open('mensagem_decifrada.txt', 'wb') as file:
-        file.write(mensagem)
-'''
+    with open(f'mensagens_cifradas/{arquivo}.txt', 'rb') as file:
+        mensagem_cifrada = file.read()
+        cipher = PKCS1_OAEP.new(chave_importada)
+        mensagem = cipher.decrypt(mensagem_cifrada)
+        print(mensagem.decode('utf-8'))
 
 
 def enviar_mensagem(texto, chave_publica, nome):
